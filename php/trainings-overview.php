c69139e9818504900c5746c19631b90e175706d8
lexvk <lex.vankeerberghen@student.howest.be>
2023-03-29 21:09:49 +0200

    Added styling for the trainingcard.php; created the trainingcard.php and used it in the trainings-overview.php file

--- php/trainings-overview.php
+++ php/trainings-overview.php
@@ -2,6 +2,27 @@
 $title = "Trasis - Trainings overview";
 include '../inc/header.php'
 ?>
-<h2>test</h2>
-<p>hello does this work</p>
-</body>
+
+<div class="flex-trainigscards">
+    <?php
+    //mockdata:
+    $titleOfTraining = "Title of training";
+    $decription = "Description of training";
+    $trainer = "Trainer of training";
+    $prerequisites = "Prerequisites of training";
+    $duration = "Duration of training";
+    $accreditation = "Accreditation of training";
+    include '../inc/trainingcard.php'?>
+    <?php
+    //mockdata:
+    $titleOfTraining = "HR Training";
+    $decription = "Description of training";
+    $trainer = "Trainer of training";
+    $prerequisites = "Prerequisites of training";
+    $duration = "Duration of training";
+    $accreditation = "Accreditation of training";
+    include '../inc/trainingcard.php'?>
+</div>
+
+
+<?php include '../inc/footer.php'?>
