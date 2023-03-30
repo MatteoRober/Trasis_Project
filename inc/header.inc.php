294b64e159684aaa02d186f9d9e13322dbaed22d
NoaDocquier <n.docquier@student.helmo.be>
2023-03-30 16:42:57 +0200

    Optimization / New architecture

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['user'])) {
             <img class="header-logo" src="../pics/h_trasis_logo.png" alt="">
             <nav class="header-nav">
                 <ul class="nav-bar">
-                    <li><a href="training.php">Dashboard</a></li>
+                    <li><a href="dashboardtraining.php">Dashboard</a></li>
                     <li><a href="available_trainings.php">Trainings</a></li>
                     <li><a href="profile.php">Profile</a></li>
                     <li><a href="php/logout.php">Logout</a></li>
