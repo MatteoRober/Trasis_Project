6d27b725e75619ec2eb26da65541983ed02ba6e1
lexvk <lex.vankeerberghen@student.howest.be>
2023-03-31 08:44:32 +0200

    sending data to DB not fully working

--- adminManagesUsers.php
+++ adminManagesUsers.php
@@ -11,7 +11,7 @@ include 'inc/header.inc.php';
 
 
 
-if(isset($_POST['validation'])) {
+if(isset($_POST['submit'])) {
     //you're here when clicking the submit button
 
     //Check if all values meet the requirements
@@ -24,10 +24,11 @@ if(isset($_POST['validation'])) {
     } elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         $message .= "Mail is not valid.";
         $noError = false;
-    } else {
+    } else { //if all values are valid
         //create new objects
         $user = new User();
         $userManagement = new UserManagement();
+
         //Generate new password
         $passwordArray = $userManagement->rand_password();
         $message = "You were not able to add a new user";
@@ -43,10 +44,11 @@ if(isset($_POST['validation'])) {
     }
 }
 ?>
+
 <div class="page-border">
     <h2>Add a new user</h2>
 
-    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']); ?>"id="addUserForm" method="post"> <!--TODO: attach function to send data to DB-->
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']); ?>" id="addUserForm" method="post"> <!--TODO: attach function to send data to DB-->
         <label for="name">Name:</label>
         <input class="inputfield" type="text" id="name" name="name" placeholder="enter the name" required>
 
@@ -59,7 +61,7 @@ if(isset($_POST['validation'])) {
         <p> The password will be auto-generated and send to the user if you click on the submit button</p>
 
 
-        <input type="submit" id="addUser" value="Add a new user" name="validation"> <!--//via php to the database -->
+        <input type="submit" name="submit" id="addUser" value="Add a new user"> <!--//via php to the database -->
         <input type="button" id="cancel" value="Cancel" onclick="clearInputs()"> <!--via php empty the form fields-->
     </form>
 </div>
