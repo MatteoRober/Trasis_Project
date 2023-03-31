acf7a164fb0da2f72b602910e5d4472d31357b35
Mattéo ROBER <m.rober@student.helmo.be>
2023-03-31 11:06:55 +0200

    small changes

--- inc/dashboardNav.inc.php
+++ inc/dashboardNav.inc.php
@@ -15,7 +15,6 @@ if (isset($_SESSION['user'])) {
 ?>
 <nav>
     <a href="dashboardTraining.php">My courses</a>
-    <a href="dashboardSummary.php">Summary completed courses</a>
     <a href="dashboardProgress.php">Progress of training courses</a>
     <a href="dashboardAccreditations.php">Accreditations</a>
     <a href="dashboardExpiringTraining.php">Expiring training courses</a>
